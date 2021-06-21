ad62a4f4ca942d0613bb3d58c931829750b0af79
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-06-21 15:36:48 +0100

    step2

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-$(#img).hide()
+
+	console.log("linked")
+		$('.sin').hide();
+		$(".list").hide();
+
+	// $('#btn').on("click",function (){
+		
+	// 	$("a").attr(,"http://www.google.com")
+	// }) 
+ 
+
+ $('.addMore').on("click",function(){
+ 	$('.sin').show();
+		$(".list").show();
+		$('#first').hide()
+		$('#btn').hide()
+ })
+
+ var Pass =  $('Password') 
+function checkMyPs(Pass){
+	
+if (Pass.length >8 &&  Pass === utilisateur[unser] ){
+
+}
+
+     }
+
+
